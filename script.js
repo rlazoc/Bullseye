93863859b8da999b292107251db68a69bce9289d
Roxana Lazo-Chavez <roxanalazo10@gmail.com>
2023-02-27 17:08:14 -0600

    create Player and Game class

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function() {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 1280;
+  canvas.height = 720;
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+    }
+  }
+
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
+    }
+  }
+
+  const game = new Game(canvas);
+  console.log(game);
+
+  function animate() {
+
+  }
+});
