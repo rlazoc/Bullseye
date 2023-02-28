87cc1f9da4b32f6c5fbe39fe3e2b0fcef98b2948
Roxana Lazo-Chavez <roxanalazo10@gmail.com>
2023-02-27 18:39:13 -0600

    Draw initial Player state in canvas

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ window.addEventListener('load', function() {
   canvas.width = 1280;
   canvas.height = 720;
 
+  ctx.fillStyle = 'white';
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = 'white';
+
   class Player {
     constructor(game) {
       this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
+    }
+    draw(context) {
+      context.beginPath();
+      context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+
+      context.restore();
+      context.stroke();
     }
   }
 
@@ -17,9 +35,13 @@ window.addEventListener('load', function() {
       this.height = this.canvas.height;
       this.player = new Player(this);
     }
+    render(context) {
+      this.player.draw(context);
+    }
   }
 
   const game = new Game(canvas);
+  game.render(ctx);
   console.log(game);
 
   function animate() {
