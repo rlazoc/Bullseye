c35c5230c720351e156e677e3d30b00411a90e20
rlazoc <rlazoc@outlook.com>
2023-03-07 23:56:10 -0600

    Update obstacles to non-overlap

--- script.js
+++ script.js
@@ -131,8 +131,26 @@ window.addEventListener("load", function () {
     }
 
     init() {
-      for (let i = 0; i < this.numberOfObstacles; i++) {
-        this.obstacles.push(new Obstacle(this));
+      let attempts = 0;
+
+      while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
+        let testObstacle = new Obstacle(this);
+        let overlap = false;
+
+        this.obstacles.forEach((obstacle) => {
+          const dx = testObstacle.collisionX - obstacle.collisionX;
+          const dy = testObstacle.collisionY - obstacle.collisionY;
+          const distance = Math.hypot(dy, dx);
+          const sumOfRadii =
+            testObstacle.collisionRadius + obstacle.collisionRadius;
+          if (distance < sumOfRadii) {
+            overlap = true;
+          }
+        });
+        if (!overlap) {
+          this.obstacles.push(testObstacle);
+        }
+        attempts++;
       }
     }
   }
