5951660f0252bdfe87229c3265d19444d80dbe44
rlazoc <rlazoc@outlook.com>
2023-03-08 16:08:29 -0600

    Implement non-passable obstacles

--- script.js
+++ script.js
@@ -62,8 +62,16 @@ window.addEventListener("load", function () {
 
       // collisions with obstacles
       this.game.obstacles.forEach((obstacle) => {
-        if (this.game.checkCollision(this, obstacle)) {
-          console.log("collision");
+        // [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
+        let [collision, distance, sumOfRadii, dx, dy] =
+          this.game.checkCollision(this, obstacle);
+        //let collision = game.checkCollision(this, obstacle)[0];
+        //let distance = game.checkCollision(this, obstacle)[1];
+        if (collision) {
+          const unit_x = dx / distance;
+          const unit_y = dy / distance;
+          this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+          this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
         }
       });
     }
@@ -164,7 +172,7 @@ window.addEventListener("load", function () {
       const dy = a.collisionY - b.collisionY;
       const distance = Math.hypot(dy, dx);
       const sumOfRadii = a.collisionRadius + b.collisionRadius;
-      return distance < sumOfRadii;
+      return [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
     }
 
     init() {
