60475571b5e7958b9f8c9ba058603a39c116a90c
rlazoc <rlazoc@outlook.com>
2023-03-12 00:12:51 -0600

    Player animation angles

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-window.addEventListener("load", function () {
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
   canvas.width = 1280;
   canvas.height = 720;
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.lineWidth = 3;
-  ctx.strokeStyle = "white";
+  ctx.strokeStyle = 'white';
 
   class Player {
     constructor(game) {
       this.game = game;
       this.collisionX = this.game.width * 0.5;
       this.collisionY = this.game.height * 0.5;
-      this.collisionRadius = 30;
+      this.collisionRadius = 50;
       this.speedX = 0;
       this.speedY = 0;
       this.dx = 0;
       this.dy = 0;
-      this.speedModifier = 5;
+      this.speedModifier = 3;
+      this.spriteWidth = 255;
+      this.spriteHeight = 255;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.spriteX;
+      this.spriteY;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.image = document.getElementById('bull');
     }
 
     draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.spriteX,
+        this.spriteY,
+        this.width,
+        this.height
+      );
       context.beginPath();
       context.arc(
         this.collisionX,
@@ -47,6 +67,18 @@ window.addEventListener("load", function () {
     update() {
       this.dx = this.game.mouse.x - this.collisionX;
       this.dy = this.game.mouse.y - this.collisionY;
+      // sprite animation
+      const angle = Math.atan2(this.dy, this.dx);
+      if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+      else if (angle < -1.96) this.frameY = 7;
+      else if (angle < -1.17) this.frameY = 0;
+      else if (angle < -0.39) this.frameY = 1;
+      else if (angle < 1.17) this.frameY = 2;
+      else if (angle < 1.96) this.frameY = 3;
+      else if (angle < 2.74) this.frameY = 4;
+      else if (angle < 2.74) this.frameY = 5;
+
+      //console.log(angle);
 
       const distance = Math.hypot(this.dy, this.dx);
       if (distance > this.speedModifier) {
@@ -59,6 +91,9 @@ window.addEventListener("load", function () {
 
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
+
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5 - 100;
 
       // collisions with obstacles
       this.game.obstacles.forEach((obstacle) => {
@@ -83,7 +118,7 @@ window.addEventListener("load", function () {
       this.collisionX = Math.random() * this.game.width;
       this.collisionY = Math.random() * this.game.height;
       this.collisionRadius = 60;
-      this.image = document.getElementById("obstacles");
+      this.image = document.getElementById('obstacles');
       this.spriteWidth = 250;
       this.spriteHeight = 250;
       this.width = this.spriteWidth;
@@ -141,19 +176,19 @@ window.addEventListener("load", function () {
       };
 
       // event listeners
-      canvas.addEventListener("mousedown", (e) => {
+      canvas.addEventListener('mousedown', (e) => {
         this.mouse.x = e.offsetX;
         this.mouse.y = e.offsetY;
         this.mouse.pressed = true;
       });
 
-      canvas.addEventListener("mouseup", (e) => {
+      canvas.addEventListener('mouseup', (e) => {
         this.mouse.x = e.offsetX;
         this.mouse.y = e.offsetY;
         this.mouse.pressed = false;
       });
 
-      canvas.addEventListener("mousemove", (e) => {
+      canvas.addEventListener('mousemove', (e) => {
         if (this.mouse.pressed) {
           this.mouse.x = e.offsetX;
           this.mouse.y = e.offsetY;
