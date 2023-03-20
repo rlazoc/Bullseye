c52f668e397672692b71b5ddb419a786629ac38e
rlazoc <rlazoc@outlook.com>
2023-03-20 12:31:49 -0500

    add game object: larva

--- script.js
+++ script.js
@@ -238,6 +238,33 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Larva {
+    constructor(game, x, y) {
+      this.game = game;
+      this.collisionX = x;
+      this.collisionY = y;
+      this.collisionRadius = 30;
+      this.image = docuement.getElementById("larva");
+      this.spriteWidth = 150;
+      this.spriteHeight = 150;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.spriteX;
+      this.spriteY;
+      this.speedY = 1 + Math.random();
+    }
+
+    draw(context) {
+      context.drawImage(this.image, this.spriteX, this.spriteY);
+    }
+
+    update() {
+      this.collisionY -= this.speedY;
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5;
+    }
+  }
+
   class Enemy {
     constructor(game) {
       this.game = game;
